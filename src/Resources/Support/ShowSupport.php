6c97bcaa27f895cec81b9e20c328bafdbde7acc9
hamzah <hamzahnafalah@gmail.com>
2025-12-22 20:16:55 +0700

    update config

--- src/Resources/Support/ShowSupport.php
+++ src/Resources/Support/ShowSupport.php
@@ -12,7 +12,22 @@ class ShowSupport extends ViewSupport
    */
   public function toArray(\Illuminate\Http\Request $request): array
   {
-    $arr = [];
+    $arr = [
+      'import_setup' => [
+        'upload_id'    => $this->upload_id,
+        'filename'     => $this->filename,
+        'chunk_size'   => $this->chunk_size,
+        'total_size'   => $this->total_size,
+        'total_chunks' => $this->total_chunks,
+        'progress'     => $this->progress,
+        'status'       => $this->status,
+        'mimetype'     => $this->mimetype,
+        'is_presigned'  => $this->is_presigned,
+        'target_path'  => $this->target_path,
+        'chunk_path'  => $this->chunk_path,
+        'chunks'     => $this->chunks,
+      ]
+    ];
     $arr = $this->mergeArray(parent::toArray($request), $arr);
     return $arr;
   }
